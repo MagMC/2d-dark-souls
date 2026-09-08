1f90f2bcf0d01d517adb93bebb77c379219117fd
MagMC <magnuschristensen42@gmail.com>
2026-09-08 20:52:10 +0200

    added walk animation and reverse

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 170.0
 const JUMP_VELOCITY = -250.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,7 +21,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.flip_h = direction < 0
+		animated_sprite.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("idle")
 
 	move_and_slide()
